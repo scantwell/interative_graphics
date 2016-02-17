b3f4f2a3d34f1d8c5d4df0185c869d8363ba007c
user <scantwell4@gmail.com>
2016-02-17 16:19:08 -0500

    Initial commit with Gourard shading implemented and all menu options. Three materials are implemented as well.

---
+++ a6/include/mesh.h
@@ -0,0 +1,44 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include "Angel.h"
+#include<vector>
+
+struct Vertex
+{
+	vec3 position;
+	vec3 normal;
+	Vertex(vec3 position, vec3 normal) : 
+		position(position),
+		normal(normal) {}
+};
+
+struct Material
+{
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	double shininess;
+	Material() {};
+	Material(vec3 ambient, vec3 diffuse, vec3 specular, double shininess) :
+		ambient(ambient),
+		diffuse(diffuse),
+		specular(specular),
+		shininess(shininess) {}
+};
+
+class Mesh 
+{
+public:
+    std::vector<Vertex> vertices;
+	std::vector<GLuint> indices;
+	Material material;
+	Mesh();
+	Mesh(std::vector<Vertex> vertices, std::vector<GLuint> indices, int primitive_type);
+    void Draw(GLuint program);
+private:
+    GLuint vao, vbo, ebo;
+    void initMesh();
+	int type; //primitive type ie GL_TRIANGLES, GL_TRIANGLE_STRIP, ..etc
+};
+#endif
---
+++ a6/readme.txt
@@ -0,0 +1,23 @@
+This assignment consists of one program.
+        - a5
+
+Features:
+
+	- Loads dragon-50000.smf simple mesh file
+	- Places two fixed white lights front left and back right.
+	- Calculates the normal for each face and adds it to the vertex normal of that face
+	- Calculate a simple Lambertain color although the machinary exists to calculate all ambient/diffuse/specular lighting.
+	- Camera Rotates around the object when toggled
+	- Camera can be adjusted by six keys (see a4 print out for details)
+	- Allows the user to choose between parallel and perspective projections.
+
+How to Run:
+
+ make
+        To compile all of source files
+
+ make run
+        To run the program.
+
+ make clean
+        Cleans all artifacts.
